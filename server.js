333b54b3740dae65961593e103bf7875535d8ad6
adityaraj1603 <aditya.20202007@mnnit.ac.in>
2023-06-15 02:28:51 +0530

    initialising project

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const colors = require("colors");
+const moragan = require("morgan");
+const dotenv = require("dotenv");
+// const connectDB = require("./config/db");
+// const path = require("path");
+
+dotenv.config();
+
+// connectDB();
+
+const app = express();
+
+app.use(express.json());
+app.use(moragan("dev"));
+
+// app.use("/api/v1/user", require("./routes/userRoutes"));
+// app.use("/api/v1/admin", require("./routes/adminRoutes"));
+// app.use("/api/v1/doctor", require("./routes/doctorRoutes"));
+
+// app.use(express.static(path.join(__dirname, "./client/build")));
+
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
+
+app.get("/", (req, res) => {
+  res.status(200).send({
+    message: "server is running",
+  });
+});
+
+const port = process.env.PORT || 7070;
+
+app.listen(port, () => {
+  console.log(
+    `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
+      .bgCyan.white
+  );
+});
